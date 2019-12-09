140e2c71f60cbb8dce96d7800561545b0d14e029
1600307376king <1600307376@qq.com>
2019-12-09 14:28:27 +0800

    up

---
+++ src/components/AdminLogin.js
@@ -0,0 +1,58 @@
+import React from "react";
+import '../assets/css/adminLogin.css'
+import axios from 'axios'
+
+
+class AdminLogin extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            admin_name: '',
+            admin_password: '',
+
+        };
+        //this.ajaxToData = this.ajaxToData.bind((this))
+    }
+
+    ajaxToData(){
+        let url = 'http://127.0.0.1:5000/admin_login/';
+        axios.post(url,  {'admin_name': 'admin', 'admin_password': '123'})
+            .then(function (response) {
+                let data = response.data;
+                console.log(data['msg'])
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }
+
+    render() {
+        return(
+
+            <div className="loginForm">
+
+                <h3>Administrator Sign In</h3>
+                <div className="nameLine">
+
+                    <input type="text" className="adminName" name="admin_name" placeholder="UserName"/>
+                </div>
+                <div className="passwordLine">
+                    <input type="password" className="adminPassword" name="admin_password" placeholder="Password"/>
+                </div>
+                <div className="buttonLine">
+                    <input className="loginButton" type="button" value="Sign In"
+                    onClick={this.ajaxToData}/>
+                </div>
+                <div className="tipLine">
+                    <input type="checkbox"/>
+                    <span className="rememberMe">Remember me</span>
+                    <a href="https://www.baidu.com" className="forgetPassword">Forget password?</a>
+                </div>
+
+            </div>
+
+        )
+    }
+}
+
+export default AdminLogin
---
+++ src/components/Base.js
@@ -0,0 +1,34 @@
+import React from "react";
+import '../assets/css/bg.css'
+import siteTitle from '../assets/images/login/site_title.png'
+import AdminLogin from "../components/AdminLogin";
+
+
+
+
+class Base extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="totalBackground" id="main">
+
+                <div className="topNav">
+                    <img src={siteTitle} alt="图片加载失败"/>
+                </div>
+                <AdminLogin/>
+                <div className="footerNav">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default Base
